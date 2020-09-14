d71530676abb7d732f5d34e695fd072022151aa7
ajspalma <palma.ashleyjs@gmail.com>
2020-09-14 19:51:25 +0800

    Added processing of register functionality and loading of user data into the app

--- src/App.js
+++ src/App.js
@@ -34,8 +34,30 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        password: '',
+        entries: 0,
+        joined: ''
+      }
     }
+  }
+
+  loadUser = (data) => {
+    const { id, name, email, password, entries, joined } = data;
+    this.setState({
+      user: {
+        id,
+        name,
+        email,
+        password,
+        entries,
+        joined
+      }
+    });
   }
 
   calculateFaceLocation = (data) => {
@@ -105,7 +127,7 @@ class App extends Component {
                 ?
                 <Signin onRouteChange={this.onRouteChange}/>
                 :
-                <Register onRouteChange={this.onRouteChange}/>  
+                <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>  
             )
         }
       </div>
